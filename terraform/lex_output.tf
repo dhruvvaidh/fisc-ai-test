9a92cbed3259ad57ec9d41a54f07cfd8b8a84157
Dhruv Vaidh <vaidhdhruv@gmail.com>
2025-04-23 23:54:51 -0400

    Revert "added new alias name"
    This reverts commit 2137afe29ad10dc7f8a278a972c407d8ee092bdf.

--- terraform/lex_output.tf
+++ terraform/lex_output.tf
@@ -5,7 +5,7 @@ data "external" "lex_alias_id" {
   program = ["bash", "-c", <<EOT
     ALIAS_ID=$(aws lexv2-models list-bot-aliases \
       --bot-id ${aws_lexv2models_bot.finance_assistant.id} \
-      --query "botAliasSummaries[?botAliasName=='financeAssistantAlias'].botAliasId | [0]" \
+      --query "botAliasSummaries[?botAliasName=='TestBotAlias'].botAliasId | [0]" \
       --output text)
 
     echo "{\"lex_bot_alias_id\": \"$ALIAS_ID\"}"
